de5d40466bf1f702917b48a1c06aa01be47924f1
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-02-28 10:26:17 +0100

    Conditional clone

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@
 #      SETUP      #
 ###################
 
-git clone https://github.com/oli4jansen/VIBE
-./VIBE/install.sh
+if [ ! -d "./VIBE" ] then
+  echo "Installing VIBE..."
+  git clone https://github.com/oli4jansen/VIBE
+  ./VIBE/install.sh
+fi
 
 ###################
 #   EXTRACTION   #
@@ -18,4 +21,7 @@ mv ./VIBE/output/* ./data/motion/
 #    RENDERING    #
 ###################
 
+if ! [ -x "$(command -v blender)" ]; then
+  echo "Installing Blender.."
+fi
 blender -t 1 -P ./generate.py -b | grep '^\[synth_motion\]'
